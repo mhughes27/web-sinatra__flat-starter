a612f4aa5279c5b99d2ddddbe1da21b99435bf12
Matt Hughes <mhughes27@gmail.com>
2016-07-25 17:13:02 -0500

    Distance almost there

--- app/controllers/conver_controller.rb
+++ app/controllers/conver_controller.rb
@@ -1,18 +1,43 @@
+require "pry"
+
+
 MyApp.get "/" do
   erb :"home"
 end
 
-MyApp.post "/convert" do
+MyApp.post "/temperature" do
+	@temperature = Temperature.new
+	@degree_c = params[:celsius].to_i
+	@degree_f = params[:fahrenheit].to_i
+		
+
+
+	erb :"/convert"
+end
+
+MyApp.post "/distance" do
+	@distance = Distance.new
+	@distance_km = params[:kilometers].to_i
+	@distance_m = params[:miles].to_i
 	
-	@degree_c = params[:celsius]
-	@degree_f = params[:fahrenheit]
-	@distance_m = params[:kilometers]
-	@distance_km = params[:miles]
+	@tomiles = @distance.km_to_m(@distance_km)
+	@tokilometers = @distance.m_to_km(@distance_m)
+	
+	erb :"/convert"
+end
+
+MyApp.post "/mass" do
+	@mass = Mass.new
 	@mass_kg = params[:kilograms]
 	@mass_lb = params[:pounds]
 	@mass_st = params[:stones]
+	
 	erb :"/convert"
 end
+
+
+
+
 
 
 # MyApp.get "/weather" do
