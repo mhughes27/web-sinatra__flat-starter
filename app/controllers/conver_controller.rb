194a300bf71ecc31feced12387f20d9f2d70b727
Matt Hughes <mhughes27@gmail.com>
2016-07-25 13:40:43 -0500

    hello world

---
+++ app/controllers/conver_controller.rb
@@ -0,0 +1,14 @@
+MyApp.get "/" do
+  erb :"home"
+end
+
+# MyApp.get "/example" do
+#   erb :"weather"
+# end
+
+# MyApp.get "/weather" do
+# 	@city = params[:name]
+# 	@weather_info = weatherData(params[:location])
+
+# 	erb :"/weather"
+# end
