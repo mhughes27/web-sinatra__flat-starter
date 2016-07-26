67028775ee99638f17d9df059c2b46315cdecf23
Matt Hughes <mhughes27@gmail.com>
2016-07-26 14:11:15 -0500

    temps good to go

--- app/controllers/conver_controller.rb
+++ app/controllers/conver_controller.rb
@@ -7,11 +7,14 @@ end
 
 MyApp.post "/temperature" do
 	@temperature = Temperature.new
-	@temp_cel = params[:"celsius"].to_f
-	@temp_fah = params[:"fahrenheit"].to_f
-
-	@toCelsius = @temperature.fah_cel(@temp_fah)
-	@toFahrenheit = @temperature.cel_fah(@temp_cel)
+	
+	if params[:"celsius"] != ""
+		@temp_cel = params[:"celsius"].to_f
+		@toFahrenheit = @temperature.cel_fah(@temp_cel)	
+	elsif params[:"fahrenheit"] != ""
+		@temp_fah = params[:"fahrenheit"].to_f
+		@toCelsius = @temperature.fah_cel(@temp_fah)		
+	end
 
 	erb :"/convert"
 end
@@ -20,10 +23,10 @@ MyApp.post "/distance" do
 	@distance = Distance.new
 	
 	if params[:kilometers] != ""
-		@distance_km = params[:kilometers].to_i
+		@distance_km = params[:kilometers].to_f
 		@toMiles = @distance.km_to_m(@distance_km)
 	elsif params[:miles] != ""
-		@distance_m = params[:miles].to_i
+		@distance_m = params[:miles].to_f
 		@toKilometers = @distance.m_to_km(@distance_m)
 	end
 	
