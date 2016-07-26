57715327bcc3b4732d19991081009230b9cd9904
Matt Hughes <mhughes27@gmail.com>
2016-07-26 14:07:47 -0500

    distance good to go

--- app/controllers/conver_controller.rb
+++ app/controllers/conver_controller.rb
@@ -18,28 +18,32 @@ end
 
 MyApp.post "/distance" do
 	@distance = Distance.new
-	@distance_km = params[:kilometers].to_f
-	@distance_m = params[:miles].to_f
 	
-	@toMiles = @distance.km_to_m(@distance_km)
-	@toKilometers = @distance.m_to_km(@distance_m)
+	if params[:kilometers] != ""
+		@distance_km = params[:kilometers].to_i
+		@toMiles = @distance.km_to_m(@distance_km)
+	elsif params[:miles] != ""
+		@distance_m = params[:miles].to_i
+		@toKilometers = @distance.m_to_km(@distance_m)
+	end
 	
 	erb :"/convert"
 end
+
 
 MyApp.post "/mass" do
 	@mass = Mass.new
 	
 	if params[:stones] != "" 
-		@mass_st = params[:stones].to_i
+		@mass_st = params[:stones].to_f
 		@pounds = @mass.stones_to_lb(@mass_st)
 		@kilograms = @mass.stones_to_kg(@mass_st)
 	elsif params[:kilograms] != "" 
-		@mass_kg = params[:kilograms].to_i
+		@mass_kg = params[:kilograms].to_f
 		@pounds = @mass.kilograms_to_lb(@mass_kg)
 		@stones = @mass.kilograms_to_st(@mass_kg)
 	elsif params[:pounds] != ""
-		@mass_lb = params[:pounds].to_i
+		@mass_lb = params[:pounds].to_f
 		@stones = @mass.pounds_to_st(@mass_lb)
 		@kilograms = @mass.pounds_to_kg(@mass_lb)
 	
